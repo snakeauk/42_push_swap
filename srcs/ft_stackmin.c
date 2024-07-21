eaf1ee3bd2df357a3a8e19706358b92be98495f3
kinamura <i.kosuke0801@gmail.com>
2024-07-21 16:29:59 +0900

    ft_stackmin.c

---
+++ srcs/ft_stackmin.c
@@ -0,0 +1,67 @@
+#include "push_swap.h"
+
+int ft_stackmin(t_stack *stack)
+{
+    int min;
+    t_stack *next;
+    
+    next = stack->next;
+    min = next->content;
+    while (next->sentinel != 1)
+    {
+        if (next->content < min)
+            min = next->content;
+        next =  next->next;
+    }
+    return (min);
+}
+// int main(int argc, char **argv)
+// {
+//     t_indata *data;
+//     t_stack *stack;
+//     int count;
+//     check_input(argc, argv);
+//     data = input_ctl(argc, argv);
+//     ft_printf("data OK!!\n\n\n");
+//     stack = input_stack(data);
+//     count = 1;
+//     stack = stack->next;
+//     while (stack->sentinel != 1)
+//     {
+//         ft_printf("%d:%d\n", count, stack->content);
+//         stack = stack->next;
+//         count++;
+//     }
+//     count--;
+//     ft_printf("stack->next OK!!\n\n\n");
+//     stack = stack->prev;
+//     while (stack->sentinel != 1)
+//     {
+//         ft_printf("%d:%d\n", count, stack->content);
+//         stack = stack->prev;
+//         count--;
+//     }
+//     ft_printf("stack->prev OK!!\n\n\n");
+//     ft_printf("====================\n\n\n");
+//     sort_3(stack, data);
+//     stack = stack->next;
+//     count = 1;
+//     while (stack->sentinel != 1)
+//     {
+//         ft_printf("%d:%d\n", count, stack->content);
+//         stack = stack->next;
+//         count++;
+//     }
+//     count--;
+//     ft_printf("stack->next OK!!\n\n\n");
+//     stack = stack->prev;
+//     while (stack->sentinel != 1)
+//     {
+//         ft_printf("%d:%d\n", count, stack->content);
+//         stack = stack->prev;
+//         count--;
+//     }
+//     ft_printf("stack->prev OK!!\n");
+//     ft_printf("min: %d", ft_stackmin(stack));
+//     exit(0);
+// }
